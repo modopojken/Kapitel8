4e397ea496c00c3a003d510f5311f768d09c4278
coel16 <modopojken@gmail.com>
2017-10-16 11:03:11 +0200

    uppgift3

---
+++ uppgift3.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class uppgift1{
+	public static void main(String[] args){
+
+		String rs;
+
+		rs = JOptionPane.showInputDialog(null, "What is the cirkles radius");
+		int r = Integer.parseInt(rs);
+		double area = Cirkel.areas(r);
+		double omkr = Cirkel.omkrs(r);
+		JOptionPane.showMessageDialog(null, "The area is: " + area + "\n" + "The circumference is: " + omkr);
+
+	}
+}
+class Cirkel{
+
+	public static double areas(int r){
+
+		return r * r * Math.PI;
+		
+
+	}
+	public static double omkrs(int r){
+		return r * 2 * Math.PI;
+	}
+
+
+}
