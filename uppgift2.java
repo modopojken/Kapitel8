3dfa2bd8c2e9c2d454f144a8a2376abd1e35e839
coel16 <modopojken@gmail.com>
2017-10-16 09:41:08 +0200

    uppgift1

---
+++ uppgift2.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class uppgift2{
+	public static void main(String[] args){
+
+		String rs;
+
+		rs = JOptionPane.showInputDialog(null, "Vad är Cirkelns radie?");
+		int r = Integer.parseInt(rs);
+		double area = Cirkel.areas(r);
+		double omkr = Cirkel.omkrs(r);
+		JOptionPane.showMessageDialog(null, "Arean är: " + area + "\n" + "Omkretsen är: " + omkr);
+
+	}
+}
+class Cirkel{
+
+	public static double areas(int r){
+
+		return r * r * Math.PI;
+		
+
+	}
+	public static double omkrs(int r){
+		return r * 2 * Math.PI;
+	}
+
+
+}
+
+
+	
